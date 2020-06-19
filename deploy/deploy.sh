051a8290d77ed7df7a830920b89eb2358bc42b5f
Pavit Tangwongkit <pavittangwongkit@MacBook-Pro.local>
2020-06-19 22:32:39 +0700

    edit travis sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,5 +2,5 @@
 git config --global push.default simple # we only want to push one branch — master
 # specify the repo on the live server as a remote repo, and name it 'production'
 # <user> here is the separate user you created for deploying
-git remote add production ssh://pepo4343@203.146.251.194:22~/ubuntu_server/fit_nodejs
+git remote add production ssh://pepo4343@203.146.251.194:22/home/pepo4343/ubuntu_server/fit/nodejs
 git push production master # push our updates
