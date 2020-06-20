df85da58ccd039ebe1aee770268de7a35b6c6d7e
Pavit Tangwongkit <pavittangwongkit@MacBook-Pro.local>
2020-06-20 19:30:32 +0700

    edit structure

--- dist/controllers/watersystem.js
+++ dist/controllers/watersystem.js
@@ -11,6 +11,14 @@ exports.listScheduleCSV = async (req, res, next) => {
     console.log();
     const csvResults = Object.values(result);
     csvResults.shift();
+    const startTimeMorning = csvResults[0].split('.').map((v, i) => {
+        return parseInt(v) * (i == 0 ? 3600 : 60);
+    });
+    const startTimeEvening = csvResults[2].split('.').map((v, i) => {
+        return parseInt(v) * (i == 0 ? 3600 : 60);
+    });
+    csvResults[0] = startTimeMorning.reduce((a, b) => a + b, 0);
+    csvResults[2] = startTimeEvening.reduce((a, b) => a + b, 0);
     res.status(201).send(csvResults.join(','));
 };
 exports.setSchedule = async (req, res, next) => {
