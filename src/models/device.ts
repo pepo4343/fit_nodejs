e7e9a8c2da11d54eec179786b4a7dce3dc5679a0
pepo4343 <ptangwongkit@gmail.com>
2020-07-10 14:38:08 +0700

    edit get device

--- src/models/device.ts
+++ src/models/device.ts
@@ -19,12 +19,50 @@ export class Device {
         if (findAlias) {
             throw Error("duplicate alias")
         }
+        const findIp = await db.findOne({ localIP: this.localIP })
+        if (findIp) {
+            throw Error("duplicate IP")
+        }
+
         return await db.insertOne(this);
     }
 
     static fetchAll = async () => {
         const db = getDb().collection(collection);
-        return await db.find({}).toArray();
+
+        const aggregate = [
+
+            {
+                $lookup: {
+                    from: "model",
+                    localField: "model_id",
+                    foreignField: "_id",
+                    as: "model",
+                },
+
+            },
+            {
+                $unwind: {
+                    path: "$model",
+                    preserveNullAndEmptyArrays: true
+                }
+            },
+            {
+                $project: {
+                    _id: "$_id",
+                    alias: "$alias",
+                    name: "$name",
+                    localIP: "$localIP",
+                    model_id: "$model_id",
+                    model_board: "$model.board",
+                    model_mcu: "$model.mcu",
+                    model_image: "$model.image",
+
+                }
+            },
+        ]
+
+        return await db.aggregate(aggregate).toArray();
     }
 
     static fetchById  = async (id: string) => {
